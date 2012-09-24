3e271029f169b46b7eb6dedf780edf3a4f3cbb13
Joshua Davis <pgmjsd@gmail.com>
2012-09-24 07:12:49 -0400

    minor cleanup

--- core/src/main/java/org/simplefix/dictionary/Dictionary.java
+++ core/src/main/java/org/simplefix/dictionary/Dictionary.java
@@ -13,28 +13,7 @@ import java.util.Map;
  */
 public class Dictionary {
 
-    public Dictionary create(org.simplefix.dictionary.jaxb.Dictionary rootElement) {
-        // The DD XML references itself by name, but we don't necessarily need to keep all that around.
-        // So, make some maps so we can resolve references.
-
-        Map<String,FieldDef> fieldsByName = Maps.newHashMap();
-
-/*
-        List<Field> fields = rootElement.getFields().getField();
-        for (Field field : fields) {
-            String name = field.getName();
-            if (fieldsByName.containsKey(name))
-                throw new RuntimeException("Duplicate field name: " + name);
-            else
-                fieldsByName.put(name,new FieldDef(field.getName(),field.getNumber().intValue(),field.getRequired()));
-        }
-
-*/
-        return new Dictionary();
-    }
-
     public class FieldDef {
-
 
     }
 }
